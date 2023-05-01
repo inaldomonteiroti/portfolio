6d0127103d0d460c79e06bfc7fdd793508b91f71
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2023-05-01 10:20:27 -0300

    agoravai

--- meusistema/autenticar.php
+++ meusistema/autenticar.php
@@ -12,18 +12,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Verifica se o usuário e a senha são válidos
     if ($usuario == $usuario_valido && $senha == $senha_valida) {
         // Autenticação bem-sucedida, inicia a sessão e redireciona para a página inicial
-   $url = $_SERVER['REQUEST_URI'];
-  
-      $link = "http://". $_SERVER['HTTP_HOST']. $url;
-      header("HTTP/1.1 301 Moved Permanently");
-      header("Location: $link");
-      exit;
-
+        session_start();
+        $_SESSION['usuario'] = $usuario;
+        header('Location: modulos/index.html');
+        exit;
     } else {
         // Autenticação falhou, exibe uma mensagem de erro
         $erro = 'Usuário ou senha inválidos.';
         echo $erro;
     }
 }
-
 ?>
